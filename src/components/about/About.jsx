b364c542eb1e1d6ca239ea0ac6e1b858e9a5af80
Juan De La Cruz <ejdelacruz@uninorte.edu.co>
2020-03-05 09:07:11 -0500

    add new image, remove imports in scss

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import { about } from "./About.module.scss";
+import SmartphoneIcon from "../../images/smartphone.svg";
 
 const About = () => {
   return (
     <div className={about} id="about">
-      <h2>Who we are</h2>
-      <h3>
-        #Aube Design is a StartUp that creates and Designs everything related
-        with the Web. Content Design, App Design, Api, Responsive Design and
-        Programming to bring every tool to the customer.
-      </h3>
+      <div className="row">
+        <div className="column">
+          <img src={SmartphoneIcon} alt="smartphone" />
+        </div>
+        <div className="column">
+          <h2>Who we are</h2>
+          <h3>
+            #Aube Design is a StartUp that creates and Designs everything
+            related with the Web. Content Design, App Design, Api, Responsive
+            Design and Programming to bring every tool to the customer.
+          </h3>
+        </div>
+      </div>
     </div>
   );
 };
